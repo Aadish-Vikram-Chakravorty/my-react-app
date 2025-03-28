4b97f5a2f10bc73ed09cad4ccf56f5f89c2b93fc
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2025-03-28 10:59:47 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -26,4 +26,24 @@ function Posts() {
   )
 }
 
+Posts.then((response) => {"http://20.244.56.144/test/users"});
+function Feed() : any {
+  return (
+    <div className="likes">
+      <h1>Top Posts</h1>
+      <p>View all the top posts</p>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="FEED"/>
+    </div>
+  )
+}
+// eslint-disable-next-line no-undef
+
+
+
 export default App;
