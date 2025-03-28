0b0fb0652d8890e662b700334f90d03cf6c310e5
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2025-03-28 10:37:56 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 function App() {
   return (
     <div className="App">
       <h1>Top Users</h1>
       <p>View all the top users</p>
-      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
-      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
-      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="USERS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="USERS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="USERS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="USERS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="USERS"/>
     </div>
   );
+}
+
+// eslint-disable-next-line no-unused-vars
+function Posts() {
+  return (
+    <div className="likes">
+      <h1>Top Posts</h1>
+      <p>View all the top posts</p>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="POSTS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="POSTS"/>
+      <image src="https://images.unsplash.com/photo-1522071820081-009f0129c71a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="POSTS"/>
+    </div>
+  )
 }
 
 export default App;
